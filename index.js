c7c29cefa5ddd70ea6bf0e7c081640c32f1ebfbf
F Nawal Abdul Careem <nawal@fs-macbook-air.home>
2020-09-21 13:00:23 +0100

    completed 3-api week 2

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+
+
+
+const db = new Pool({
+    user: 'nawal',
+	host: 'localhost',
+	database: 'cyf_ecommerce',
+	password: 'open123',
+	port: 5432
+});
+
+app.get("/customers",(req, res)=>{
+db.query("SELECT id, name, address, city, country FROM customers",
+    (error, result) => {
+      console.log(result)
+      res.json(result.rows);
+  });
+})
+
+app.get("/suppliers",(req, res)=>{
+    db.query("SELECT * FROM suppliers",
+        (error, result) => {
+          console.log(result)
+          res.json(result.rows);
+      });
+    })
+
+
+    app.get("/products",(req, res)=>{
+    const sql = 'SELECT p.product_name, pa.unit_price, s.supplier_name from product_availability pa join products p on pa.prod_id = p.id join suppliers s on pa.supp_id = s.id';
+    
+    db.query(sql, [],
+        (error, result) => {
+          console.log(result)
+          res.json(result.rows);
+      });
+      
+    })
+
+
+
+app.listen(3000, function() {
+	console.log("Server is listening on port 3000.");
+});
